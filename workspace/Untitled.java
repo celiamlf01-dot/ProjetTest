39fba3e64206a78a00a9bd464dee1a4d0a39f5f3
celia-m1 <cylia.makhlouf@ensia.edu.dz>
2025-09-24 21:24:42 +0200

    java file'

---
+++ workspace/Untitled.java
@@ -0,0 +1,17 @@
+public class Untitled {
+    private string name;
+    private int health;
+    private int attackPower;
+    private 
+    Untitled(string name,int health,int attackPower){
+        this.name=name;
+        this.health=health>0?health:0;
+        this.attackPower=attackPower>0?attackPower:0;
+
+
+    }
+
+
+
+    
+}
